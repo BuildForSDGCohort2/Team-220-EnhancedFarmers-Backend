21cc30fdea7185302ad04a1bc8cf4586ad159c53
MUGABA <mugabamuha@gmail.com>
2020-09-29 21:51:25 +0300

    Trigger Heroku deploy after enabling collectstatic

--- src/app.js
+++ src/app.js
@@ -1,19 +1,8 @@
 import express from "express";
 import logger from "./helpers/logger";
-import t from "./database/tables";
 
 const app = express();
 require("./startup/routes")(app);
-
-// t.createTableProfessional();
-// t.createTableFarmers();
-// t.createTableInvestors();
-// t.createTableCustomers();
-// t.createTableProjects();
-// t.createTableProducts();
-// t.createEnum();
-// t.createTableOrders();
-t.createAdmin();
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, logger.info(`listening to port ${port}`));
