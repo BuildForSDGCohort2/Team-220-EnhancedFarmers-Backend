bcdf1523732c73746e75f113f2e5f7c78f89011f
MUGABA <mugabamuha@gmail.com>
2020-09-29 22:27:19 +0300

    just pushing with changed database

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ require("./startup/routes")(app);
 // t.createTableProjects();
 // t.createTableProducts();
 // t.createTableOrders();
-t.createAdmin();
+// t.createAdmin();
+t.createTableProducts();
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, logger.info(`listening to port ${port}`));
