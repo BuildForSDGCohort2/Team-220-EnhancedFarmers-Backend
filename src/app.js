82c0700e06af0e970b718429ce24beef3d2f10d9
MUGABA <mugabamuha@gmail.com>
2020-09-29 21:20:45 +0300

    just pushing with changed database

--- src/app.js
+++ src/app.js
@@ -5,14 +5,15 @@ import t from "./database/tables";
 const app = express();
 require("./startup/routes")(app);
 
-t.createTableProfessional();
-t.createTableFarmers();
-t.createTableInvestors();
-t.createTableCustomers();
-t.createTableProjects();
-t.createTableProducts();
-t.createEnum();
-t.createTableOrders();
+// t.createTableProfessional();
+// t.createTableFarmers();
+// t.createTableInvestors();
+// t.createTableCustomers();
+// t.createTableProjects();
+// t.createTableProducts();
+// t.createEnum();
+// t.createTableOrders();
+t.createAdmin();
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, logger.info(`listening to port ${port}`));
