b41db9cfa78aa89ae6e8c32b491eccd135f19d48
MUGABA <mugabamuha@gmail.com>
2020-09-29 22:11:17 +0300

    just pushing with changed database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from "express";
 import logger from "./helpers/logger";
+import t from "./database/tables";
 
 const app = express();
 require("./startup/routes")(app);
+
+t.createEnum();
+t.createTableCustomers();
+t.createTableFarmers();
+t.createTableProfessional();
+t.createTableInvestors();
+t.createTableProjects();
+t.createTableProducts();
+t.createTableOrders();
+t.createAdmin();
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, logger.info(`listening to port ${port}`));
